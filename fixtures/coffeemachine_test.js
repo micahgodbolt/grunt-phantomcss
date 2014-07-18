6a769c91c76f79ebdba49060dc6c9efa5382919d
Anselm Hannemann <info@anselm-hannemann.com>
2014-07-18 11:16:42 +0200

    Update visual regression test screenshots.

--- fixtures/coffeemachine_test.js
+++ fixtures/coffeemachine_test.js
@@ -1,28 +1,28 @@
-casper.start('fixtures/coffeemachine.html').then(function(){
+casper.start('fixtures/coffeemachine.html').then(function () {
     phantomcss.screenshot('#coffee-machine-wrapper', 'open coffee machine button');
-}).then(function(){
+}).then( function () {
     casper.click('#coffee-machine-button');
 
     casper.waitForSelector('#myModal:not([style*="display: none"])',
-        function success(){
+        function success () {
             phantomcss.screenshot('#myModal', 'coffee machine dialog');
         },
-        function timeout(){
+        function timeout () {
             casper.test.fail('Should see coffee machine');
         }
     );
-}).then(function(){
+}).then( function () {
     casper.click('#cappuccino-button');
     phantomcss.screenshot('#myModal', 'cappuccino success');
-}).then(function(){
+}).then( function () {
     casper.click('#close');
 
     // wait for modal to fade-out
     casper.waitForSelector('#myModal[style*="display: none"]',
-        function success(){
+        function success () {
             phantomcss.screenshot('#coffee-machine-wrapper', 'coffee machine close success');
         },
-        function timeout(){
+        function timeout () {
             casper.test.fail('Should be able to walk away from the coffee machine');
         }
     );
